c8affb1c23770be21ae60d2c2c3aa0bff95c5862
sulifer <937096063@qq.com>
2020-04-30 09:46:18 +0800

    'asdasd'

--- public/index.php
+++ public/index.php
@@ -16,7 +16,9 @@ namespace think;
 require __DIR__ . '/../thinkphp/base.php';
 
 // 支持事先使用静态方法设置Request对象和Config对象
-header("Access-Control-Allow-Origin: *");
+$origin=$_SERVER['origin'];
+header("Access-Control-Allow-Origin: $origin");
+header("Access-Control-Allow-Credentials","true");
 header("Access-Control-Allow-Methods:GET, POST, OPTIONS, DELETE");
 header("Access-Control-Allow-Headers:DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type, Accept-Language, Origin, Accept-Encoding");
 // 执行应用并响应
